4621ce6fd271ed108e31080c781cacd7865bca53
Ishayahu Lastov <ishayahu@mail.ru>
2013-02-04 13:31:28 +0400

    ДобавленÐы файлы версии

--- tasks/settings.py
+++ tasks/settings.py
@@ -46,7 +46,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/usr/home/ishayahu/tasks/tasks/files/'
+MEDIA_ROOT = '/usr/home/ishayahu/tasks/todoes/files/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
